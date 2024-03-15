a3ed749f481d94e9fcda388c86fdaf8e5c44bf90
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-15 15:10:04 +0300

    Исправление архитектуры приложения

--- lib/models/MyFiles.dart
+++ lib/models/MyFiles.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard/constants.dart';
+import 'package:responsive_dashboard/theme/constants.dart';
 
 class CloudStorageInfo {
   final String svgSrc, title, totalStorage;
