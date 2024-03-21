607f0f53e1e13350ee6947b9f11f12fa249acfe7
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-21 12:45:25 +0300

    Разделение экрана на области

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/screens/components/side_menu.dart';
+import 'package:responsive_dashboard/screens/dashboard/dashboard_screen.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         child: Row(
           children: [
-            const Expanded(
+            Expanded(
               child: SideMenu(),
             ),
             Expanded(
               flex: 5,
-              child: Container(
-                color: Colors.blue,
-              ),
+              child: DashboardScreen(),
             ),
           ],
         ),
