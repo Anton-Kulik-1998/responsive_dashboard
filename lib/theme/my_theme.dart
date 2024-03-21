e42db11810abc26f9b970781b208d9e27eb84520
Антон Кулик <anton.smile.1998@gmail.com>
2024-03-21 12:07:17 +0300

    Исправлен цвет текста

--- lib/theme/my_theme.dart
+++ lib/theme/my_theme.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_dashboard/theme/constants.dart';
 
+TextTheme textTheme = const TextTheme(
+  headlineLarge: TextStyle(
+    color: Colors.white,
+  ),
+);
+
 ThemeData myTheme = ThemeData.dark().copyWith(
   scaffoldBackgroundColor: bgColor,
-  textTheme: GoogleFonts.poppinsTextTheme().apply(bodyColor: Colors.white),
+  textTheme:
+      GoogleFonts.poppinsTextTheme(textTheme).apply(bodyColor: Colors.white),
   canvasColor: secondaryColor,
 );
